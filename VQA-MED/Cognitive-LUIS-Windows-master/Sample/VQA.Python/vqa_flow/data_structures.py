44ed96c0817791a02ce5e8b387f1b23d05ebcb99
turner11 <avi.turner111@gmail.com>
2018-06-05 20:59:37 +0300

    WIP: Update to notebook

--- VQA-MED/Cognitive-LUIS-Windows-master/Sample/VQA.Python/vqa_flow/data_structures.py
+++ VQA-MED/Cognitive-LUIS-Windows-master/Sample/VQA.Python/vqa_flow/data_structures.py
@@ -18,4 +18,10 @@ class EmbeddingData(object):
         self.meta_data = meta_data
 
     def __repr__(self):
-        return super().__repr__()
+        return f"{self.__class__.__name__}({self.embedding_matrix},{self.embedding_dim},{self.seq_length}, {self.meta_data})"
+        # return super().__repr__()
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__:}(Embedding length:{len(self.embedding_matrix)}, Embedding dim: {self.embedding_dim}, seq length: {self.seq_length}, meta length: {len(self.meta_data)})"
+
+
