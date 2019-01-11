fd476dea4b7418e41f57cd0779e9ef22f27bf51c
turner11 <avi.turner111@gmail.com>
2019-01-11 16:01:07 +0200

    using fixture in test

--- VQA-MED/VQA.Python/tests/test_model.py
+++ VQA-MED/VQA.Python/tests/test_model.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 import pandas as pd
 import logging
 from classes.vqa_model_builder import VqaModelBuilder
@@ -6,6 +7,15 @@ from classes.vqa_model_predictor import VqaModelPredictor
 from classes.vqa_model_trainer import VqaModelTrainer
 from common.utils import VerboseTimer
 logger = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def data_frame():
+    folder, _ = os.path.split(__file__)
+    data_path = os.path.abspath(os.path.join(folder,'data_for_test/train_data.hdf'))
+    with pd.HDFStore(data_path) as store:
+        train_data = store['data']
+    return train_data
 
 loss, activation = 'categorical_crossentropy', 'sigmoid'
 
@@ -22,22 +32,21 @@ def test_model_creation():
         assert hasattr(model, field), f'A model is expected to have a "{field}" attribute'
 
 
-def test_model_training():
+def test_model_training(data_frame):
     batch_size = 75
     mb = VqaModelBuilder(loss, activation)
     model = mb.get_vqa_model()
 
-    folder,_ = os.path.split(__file__)
-    data_path = os.path.abspath(os.path.join(folder,'data_for_test/train_data.hdf'))
-    with pd.HDFStore(data_path) as store:
-        train_data = store['data']
+    train_data = data_frame
 
     mt = VqaModelTrainer(model, use_augmentation=False, batch_size=batch_size, data_location=train_data)
     mt.train()
 
-    mp = VqaModelPredictor(model)
-    test_data = train_data
-    predictions = mp.predict(train_data)
+def test_model_predicting(data_frame):
+    model_path = 'C:\\Users\\avitu\\Documents\\GitHub\\VQA-MED\\VQA-MED\\VQA.Python\\tests\\data_for_test\\test_model\\vqa_model_.h5'
+    mp = VqaModelPredictor(model_path)
+    test_data = data_frame
+    predictions = mp.predict(test_data)
     preds = predictions.prediction
     # ground_truth = predictions.answer
     assert len(preds) == len(test_data), 'Got a different number for predictions input and output'
