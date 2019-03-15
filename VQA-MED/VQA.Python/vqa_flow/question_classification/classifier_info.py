fb0e588bef1b8421a3736d0d9dd2fc84b72e0dd0
turner11 <avi.turner111@gmail.com>
2019-03-15 09:12:11 +0200

    Calssifier info returns a figure so it can be saved

--- VQA-MED/VQA.Python/vqa_flow/question_classification/classifier_info.py
+++ VQA-MED/VQA.Python/vqa_flow/question_classification/classifier_info.py
@@ -4,11 +4,12 @@ import matplotlib.pyplot as plt
 
 class ClassifierInfo(object):
 
-    def __init__(self, classifier, scores_train, scores_test) -> None:
+    def __init__(self, classifier, scores_train, scores_test, classes) -> None:
         super().__init__()
         self.classifier = classifier
         self.scores_train = scores_train
         self.scores_test = scores_test
+        self.classes = classes
 
     @property
     def accuracy(self):
@@ -35,13 +36,13 @@ class ClassifierInfo(object):
     def __get_score(self, metric, metric_container):
         return metric_container[metric][-1]
 
-    def plot(self, title=None, block=False):
+    def get_figure(self, title=None, plot=False):
         title = title if title is not None else f'Hidden layers: {self.classifier.hidden_layer_sizes}'
-        self.plot_classifier_scores(self.scores_test, self.scores_train, title=title, block=block)
+        return self.get_classifier_scores_figure(self.scores_test, self.scores_train, title=title, plot=plot)
 
     @staticmethod
-    def plot_classifier_scores(test_vals, train_vals, title='', block=False):
-
+    def get_classifier_scores_figure(test_vals, train_vals, title='', plot=False):
+        fig = None
         try:
 
             major_ticks = np.arange(0, 1.1, 0.1)
@@ -65,12 +66,15 @@ class ClassifierInfo(object):
                     col.set_yticks(major_ticks)
                     col.set_yticks(minor_ticks, minor=True)
 
-                    col.set(xlabel='Epochs', ylabel=name, title=f"{name} over epochs", ylim=[0, 1])  # set_xlim =[0, 5]
+                    col.set(xlabel='Epochs', ylabel=name, title=f"{name} over epochs {scores_test[-1]:.3f}", ylim=[0, 1])  # set_xlim =[0, 5]
             fig.legend(loc='upper left')
             fig.suptitle(str(title), fontsize=20)
 
-            plt.pause(0.001)
-            plt.show()
 
         except Exception as ex:
             print(ex)
+
+        if plot:
+            plt.pause(0.001)
+            plt.show()
+        return fig
