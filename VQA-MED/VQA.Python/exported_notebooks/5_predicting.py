a4c45d6f0fc027715d981eb294f023db15fd0dab
turner11 <avi.turner111@gmail.com>
2019-03-28 20:02:04 +0200

    Notebook

--- VQA-MED/VQA.Python/exported_notebooks/5_predicting.py
+++ VQA-MED/VQA.Python/exported_notebooks/5_predicting.py
@@ -12,9 +12,12 @@ import numpy as np
 
 
 from classes.vqa_model_predictor import VqaModelPredictor, DefaultVqaModelPredictor
+from data_access.api import DataAccess, SpecificDataAccess
 from common.DAL import get_models_data_frame, get_model
 from common.DAL import ModelScore
+from common.settings import data_access as data_access_api
 from common import DAL
+
 
 from evaluate.VqaMedEvaluatorBase import VqaMedEvaluatorBase
 from common.functions import get_highlighted_function_code
@@ -22,6 +25,13 @@ import vqa_logger
 
 
 # In[3]:
+
+
+from keras import backend as keras_backend
+keras_backend.clear_session()
+
+
+# In[4]:
 
 
 df_models = get_models_data_frame()
@@ -36,7 +46,7 @@ except KeyError: #if no scode yet
 df_show.tail()
 
 
-# In[4]:
+# In[5]:
 
 
 import logging
@@ -45,36 +55,62 @@ logger = logging.getLogger(__name__)
 import IPython
 
 
-# In[5]:
+# In[6]:
 
 
-model_id = None#int(model_id)
-mp = DefaultVqaModelPredictor(model_id)
+model_id = 5#int(model_id)
+model_folder = 'C:\\Users\\Public\\Documents\\Data\\2019\\models\\20190315_1823_38\\'
+
+question_category = 'Abnormality'
+data_access = SpecificDataAccess(data_access_api.folder, question_category=question_category, group=None)
+# SpecificDataAccess?
+
+
+model_arg = model_folder
+mp = DefaultVqaModelPredictor(model_arg,data_access=data_access)
 mp
 
 
-# In[6]:
+# In[7]:
 
 
 mp.df_validation.head(2)
 
 
-# In[7]:
+# In[8]:
 
 
 code = get_highlighted_function_code(mp.predict,remove_comments=False)
 IPython.display.display(code)
 
 
-# In[8]:
-
-
-df_data = mp.df_validation
-df_predictions = mp.predict(mp.df_validation)
-df_predictions.head()
-
-
 # In[9]:
+
+
+df_validation = mp.df_validation
+df_train = data_access.load_processed_data(group='train')
+
+
+# In[10]:
+
+
+validation_set = df_train
+
+
+# In[11]:
+
+
+validation_set.sample(3)
+
+
+# In[12]:
+
+
+df_predictions = mp.predict(validation_set)
+df_predictions.sample(5)
+
+
+# In[ ]:
 
 
 df_predictions.describe()
@@ -82,7 +118,7 @@ df_predictions.describe()
 
 # #### Take a look at results for a single image:
 
-# In[85]:
+# In[ ]:
 
 
 
@@ -92,20 +128,29 @@ df_image = df_predictions[df_predictions.image_name == image_name]
 # print(f'Result: {set(df_image.prediction)}')
 
 image_path = df_image.path.values[0]
+
+def get_row_evaluation(row, metric):
+    return VqaMedEvaluatorBase.get_all_evaluation(predictions=[row.prediction], ground_truth=[row.answer])[metric]
+
+sorted_cols = sorted(df_image.columns, key=lambda s: s not  in ['answer', 'prediction'])
+df_image = df_image[sorted_cols]
+
+df_image['wbss'] = df_image.apply(lambda row: get_row_evaluation(row, 'wbss'), axis=1)
+df_image['bleu'] = df_image.apply(lambda row: get_row_evaluation(row, 'bleu'), axis=1)
 df_image
 
 
-# In[86]:
+# In[ ]:
 
 
 from IPython.display import Image, HTML, display_html
-image = Image(filename = image_path, width=400, height=400)
+image = Image(filename = image_path, width=600, height=600)
 image
 
 
 # ## Evaluating the Model
 
-# In[13]:
+# In[ ]:
 
 
 validation_prediction = df_predictions
@@ -115,9 +160,13 @@ results = VqaMedEvaluatorBase.get_all_evaluation(predictions=predictions, ground
 print(f'Got results of\n{results}')
 
 
-# ##### Add the core to DB:
+# In[ ]:
 
-# In[14]:
+
+# validation_prediction.sort_values(by=['probabilities'], ascending=False)
+
+
+# In[36]:
 
 
 model_db_id = mp.model_idx_in_db
@@ -125,7 +174,7 @@ assert model_db_id >= 0
 model_db_id
 
 
-# In[15]:
+# In[17]:
 
 
 bleu = results['bleu']
@@ -134,8 +183,10 @@ model_score = ModelScore(model_db_id, bleu=bleu, wbss=wbss)
 model_score
 
 
-# In[16]:
+# ##### Add the score to DB:
+
+# In[18]:
 
 
-DAL.insert_dal(model_score)
+# DAL.insert_dal(model_score)
 
